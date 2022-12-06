bef1272a706e50c7585a78cac441a9d6364177d4
Guilherme Assis <60708165+ManoAssis@users.noreply.github.com>
2022-12-06 11:50:02 -0300

    Create rodape.php

---
+++ rodape.php
@@ -0,0 +1,33 @@
+
+
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <div class="container-fluid">
+    <nav class="navbar navbar-dark bg-dark">
+    <div class="container">
+      <a class="navbar-brand" href="./">
+        <img src="./img/logo.png" alt="" width="30" height="24">
+      </a>
+    </div>
+    </nav>
+
+    <div class="collapse navbar-collapse" id="navbarNavDropdown">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="questionario.php">Questionario</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="perguntas.php">Perguntas</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="alternativas.php">Alternativas</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+
+
+<link rel="stylesheet" href="footer.css">
+<script src="/DevSi/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
