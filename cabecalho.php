1018d8094c68b731965f6fdeb0cd5b513e08d7e9
Guilherme Assis <60708165+ManoAssis@users.noreply.github.com>
2022-12-06 11:48:27 -0300

    Create cabecalho.php

---
+++ cabecalho.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/geradorprovas/css/bootstrap.min.css" />
+    <link rel="stylesheet" href="footer.css">
+    <title>Prova Ferneko</title>
+</head>
+<body>
+
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <div class="container-fluid">
+  <nav class="navbar navbar-dark bg-dark">
+  <div class="container">
+    <a class="navbar-brand" href="./">
+      <img src="./img/logo.png" alt="" width="30" height="24">
+    </a>
+  </div>
+</nav>
+    <div class="collapse navbar-collapse" id="navbarNavDropdown">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="questionario.php">Questionario</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="perguntas.php">Perguntas</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="alternativas.php">Alternativas</a>
+        </li>
+        
+      </ul>
+    </div>
+  </div>
+</nav>
